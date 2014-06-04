88f32bbbfac5eaa42d984cfbab95400dd0aea7c2
Andrey Elenskiy <andreye@wix.com>
2014-06-04 12:09:55 -0700

    Fixes port

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 var app = require('./index');
 
 app.listen(process.env.PORT || 3000, function() {
-    console.log('Listening on port %d', server.address().port);
+    console.log('Listening on port %d', process.env.PORT);
 });
